36b970e80e677d801ca55f1b6ea0eec2f23f51fb
Leandro Ferrero <learferrero@gmail.com>
2026-04-01 21:18:44 -0300

    Comenzando a llenar el README

--- README.m
+++ README.m
@@ -1,1 +1,81 @@
 # frontend-pfo1
+<h1>Frontend PFO1: Portafolio Personal</h1>
+<h2>Sobre el Proyecto</h2>
+<p>
+    Este proyecto es mi Práctica Formativa Obligatoria 1 (PFO1) de frontend. 
+    Consiste en una landing page personal que explica un poco quién soy, 
+    mi aprendizaje por el mundo IT y algunos de mis hobbies. 
+    Todo bajo una estética inspirada en los manuales de Dungeons & Dragons. En definitiva, es un pequeño manual de quién soy.
+</p>
+
+<h2>Checklist - Práctica Formativa Obligatoria 1</h2>
+
+<h3>Estructura del Proyecto:</h>
+<ul>
+    <li>[ ] Archivo "index.html" ubicado en la raíz.</li>
+    <li>[ ] Carpeta "css" que contenga el archivo "styles.css".</li>
+    <li>[ ] (Opcional) Carpeta "img" para recursos gráficos.</li>
+    <li>[ ] Archivo "README.md" creado, que incluya una breve descripción del TP y este checklist.</li>
+</ul>
+
+<h3>Repositorio y Publicación:</h3>
+<ul>
+    <li>[ ] Repositorio en GitHub creado.</li>
+    <li>[ ] Proyecto subido al repositorio.</li>
+    <li>[ ] Proyecto publicado utilizando GitHub Pages.</li>
+    <li>[ ] En el "README.md" se indica la URL de GitHub Pages.</li>
+    <li>• Uso de Google Fonts:</li>
+    <li>[ ] Enlace a Google Fonts incluido en la sección "head" del HTML.</li>
+    <li>[ ] La tipografía importada se aplica en el sitio.</li>
+    <li>[ ] Redacta brevemente tu decisión: ¿Por qué elegiste esa fuente?
+        <p>
+            Respuesta: Use 3 fuenes: Cinzel, Lora y Fira sans. Las use porque me base en la estetica del manual de Dungeons and Drangos para el proyecto.
+            Y esas fuentes encajaban pefectamente con el estilo visual que le queria dar.
+        </p>    
+    </li>
+</ul>
+
+<h3>• HTML:</h3>
+<ul>
+    <li>[ ] El documento inicia con la declaración DOCTYPE y usa el atributo lang="es".</li>
+    <li>[ ] Se han incluido las metaetiquetas obligatorias: charset y viewport.</li>
+    <li>[ ] Se ha definido un título descriptivo.</li>
+    <li>[ ] Se han vinculado correctamente el archivo CSS y el enlace a Google Fonts.</li>
+    <li>Secciones obligatorias en "main":</li>
+    <li>[ ] Barra de navegación ("nav") presente y contiene al menos 3 enlaces.</li>
+    <li>[ ] Se han insertado al menos 4 comentarios explicativos en el código HTML.</li>
+</ul>
+
+<h3>CSS:</h3>
+<ul>
+    <li>[ ] Existe el archivo "styles.css" con estilos personalizados.</li>
+    <li>[ ] Se utilizan selectores basados en clases e identificadores.</li>
+    <li>[ ] La tipografía importada desde Google Fonts se aplica correctamente en todos los elementos.</li>
+    <li>Layout y Organización:</li>
+    <li>[ ] Se ha organizado el layout (especialmente en la sección "tarjetas") utilizando Flexbox o Grid.</li>
+    <li>[ ] Redacta: ¿Qué ventajas encontraste al utilizar Flexbox o Grid en tu proyecto?
+        <>
+            Respuesta: Elegí Flexbox porque me resultó mucho más sencillo de aprender que Grid. 
+            La mayor ventaja fue la facilidad con la que pude alinear el contenido y distribuir los espacios de forma dinámica.
+        </p>
+    </li>
+</ul>
+
+<h3>Estilización de Componentes:</h3>
+<ul>
+    <li>[ ] Se han personalizado los estilos de tablas, botones, enlaces y formularios.</li>
+    <li>[ ] Se han ajustado las dimensiones de imágenes y contenedores utilizando unidades relativas (%, rem, vh).</li>
+    <li>[ ] Se ha implementado al menos una animación o transición (por ejemplo, efecto hover en tarjetas o botones).</li>
+    <li>[ ] Redacta: ¿Qué animación o transición implementaste y por qué consideraste que era adecuada para tu proyecto?
+        <p> 
+            Respuesta:Implementé transiciones de all 0.5s y transformaciones de scale(0.9) con box-shadow en el estado :hover de las tarjetas de proyectos y las imágenes. 
+            Consideré que eran adecuadas porque aportan, además de mejorar la experiencia de usuario al resaltar visualmente el elemento seleccionado.
+        </p>
+    </li>
+</ul>
+<h3>Consideraciones Adicionales:</h3>
+<ul>
+    <li>[ ] El diseño es responsivo y se visualiza correctamente en distintos dispositivos.</li>
+    <li>[ ] Se aplicaron buenas prácticas de accesibilidad (por ejemplo, uso adecuado de atributos alt en las imágenes).</li>
+    <li>[ ] Se añadieron comentarios adicionales donde se describan decisiones de diseño o la lógica de implementación.</li>
+</ul>
